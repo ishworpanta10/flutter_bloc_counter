11f4018b2d35085294d9fb0d406c132d6f0573cd
ishworpanta10 <ishworpanta10@gmail.com>
2020-12-02 13:13:55 +0545

    ✅ flutter bloc cuprit fake weather app

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,20 @@
-import 'package:bloc_demo/Bloc/home.dart';
+import 'package:bloc_demo/bloc/weather_bloc.dart';
+import 'package:bloc_demo/data/weather_repository.dart';
+import 'package:bloc_demo/pages/weather_search_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      title: 'Material App',
+      home: BlocProvider(
+        create: (context) => WeatherBloc(FakeWeatherRepository()),
+        child: WeatherSearchPage(),
       ),
-      home: MyHomePage(title: 'Flutter Demo Bloc Example'),
     );
   }
 }
